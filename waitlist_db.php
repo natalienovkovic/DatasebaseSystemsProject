ed99571301cd7ea94046f8b81bbb8c2c023076b9
ct4wa <60021401+ct4wa@users.noreply.github.com>
2020-11-11 22:12:40 -0500

    Add files via upload

---
+++ waitlist_db.php
@@ -0,0 +1,57 @@
+<?php 
+
+// CREATE TABLE friends (
+//    name varchar(30) NOT NULL,
+//    major varchar(10) NOT NULL,
+//    year int NOT NULL,
+//    PRIMARY KEY (name) );  
+
+// Prepared statement (or parameterized statement) happens in 2 phases:
+//   1. prepare() sends a template to the server, the server analyzes the syntax
+//                and initialize the internal structure.
+//   2. bind value (if applicable) and execute
+//      bindValue() fills in the template (~fill in the blanks.
+//                For example, bindValue(':name', $name);
+//                the server will locate the missing part signified by a colon
+//                (in this example, :name) in the template
+//                and replaces it with the actual value from $name.
+//                Thus, be sure to match the name; a mismatch is ignored.
+//      execute() actually executes the SQL statement
+
+
+function addToWaitlist($sid, $listingID)
+{
+
+  global $db;
+  $query = "INSERT INTO Waitlist VALUES(:sid, :listingID, NULL)";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':sid', $sid);
+  $statement->bindValue(':listingID', $listingID);
+  $statement->execute();
+
+  $results = $statement->fetchAll(); // returns an array of rows
+  $statement->closeCursor();
+	
+}
+
+
+function getPosition($sid, $listingID)
+{
+
+  global $db;
+  $query = "SELECT * FROM Waitlist WHERE sid=:sid AND listingID=:listingID";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':sid', $sid);
+  $statement->bindValue(':listingID', $listingID);
+  $statement->execute();
+
+  $results = $statement->fetchAll(); // returns an array of rows
+  $statement->closeCursor();
+  
+  return $results;
+	
+}
+
+
+
+?>
